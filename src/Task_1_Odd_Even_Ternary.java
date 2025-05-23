c57c197b53afb4cf68da5055005cf9f9c1b982af
diggu <kunjirdiggu@gmail.com>
2025-05-24 01:18:42 +0530

    Assignments completed

--- src/Task_1_Odd_Even_Ternary.java
+++ src/Task_1_Odd_Even_Ternary.java
@@ -4,6 +4,7 @@ public class Task_1_Odd_Even_Ternary
 {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
+
         System.out.println("Enter any integer number to check whether it is odd or even");
         int var = sc.nextInt();
         String res = (var % 2 == 0) ?"Even" : "Odd";
